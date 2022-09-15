6e326c70d4660c5eb70781fbdf15e60a1d2fe224
unknown <x_qi@trip.com>
2022-09-15 10:14:38 +0800

    feat: learn-wxml

--- miniprogram/pages/index/index.ts
+++ miniprogram/pages/index/index.ts
@@ -7,7 +7,8 @@ Page({
     pages: [
       { name: '01_注册页面', path: '/pages/01_注册页面/index' },
       { name: '02_常见组件', path: '/pages/02_常见组件/index' },
-      { name: '03_学习WXSS', path: '/pages/03_learn_wxss/index' }
+      { name: '03_学习WXSS', path: '/pages/03_learn_wxss/index' },
+      { name: '04_学习WXML', path: '/pages/04_learn_wxml/index' }
     ]
   },
   onBtnClick(e) {
